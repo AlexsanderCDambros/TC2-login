0bf69924adf35495ec4967e1c7ff16bc9d777bf2
Alexsander Chagas Dambros <alexsanderdambros@gmail.com>
2025-08-01 16:12:50 -0300

    chore: teste MUI

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,18 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Button, TextField } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <>
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <Button variant="outlined" startIcon={<DeleteIcon />}>
+        Delete
+      </Button>
+      <section>{props.name} is mounted!</section>
+    </>
+  );
 }
